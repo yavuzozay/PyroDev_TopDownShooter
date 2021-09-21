9b3318e88e91649b6cb331d13c1da2293ae1f783
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-09-21 11:42:12 +0300

    Update CamStickController.cs

--- Assets/02_Components/Camera/CamStickController.cs
+++ Assets/02_Components/Camera/CamStickController.cs
@@ -14,12 +14,16 @@ public class CamStickController : MonoBehaviour
         mT = transform;
         curPos = mT.position;
     }
-
-
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        curPos = Vector3.Lerp(curPos, AimPos, Time.deltaTime * 3);
-
+        curPos = Vector3.Lerp(curPos, AimPos, Time.fixedDeltaTime * 3);
         mT.position = curPos;
     }
+
+    /*   private void LateUpdate()
+       {
+         curPos = Vector3.Lerp(curPos, AimPos, Time.deltaTime * 3);
+
+           mT.position = curPos;
+       }*/
 }
